6dfa0f734468af54a7442e471d7bec9539654466
BrokeCoder1 <matthew.lowe87911@outlook.com>
2024-11-11 09:38:55 +0200

    Update Report.cs
    Summary report~Matthew

--- Group26_PRG2782_Project/Business logic layer/Report.cs
+++ Group26_PRG2782_Project/Business logic layer/Report.cs
@@ -1,61 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Group26_PRG2782_Project.Business_logic_layer
 {
     internal class Report
-    { 
-        {
+    {
         private List<Student> students;
- 
-        public ReportForm(List<Student> students)
+
+        public Report(List<Student> students)
         {
-            InitializeComponent();
             this.students = students;
- 
-            
-            DisplaySummary();
         }
- 
-        private void DisplaySummary()
+
+       
+        public void GenerateSummaryReport()
         {
             int totalStudents = students.Count;
- 
-            
             double averageAge = students.Any() ? students.Average(s => s.Age) : 0;
- 
+
             
-            txtTotalStudents.Text = totalStudents.ToString();
-            txtAverageAge.Text = averageAge.ToString("F2");
- 
-            
-            SaveSummaryToFile(totalStudents, averageAge);
-        }
- 
-        private void SaveSummaryToFile(int totalStudents, double averageAge)
-        {
-            string filePath = @"C:\Users\sophi\Desktop\PRG272\ReportSummary.txt";
- 
- 
+            string filePath = "summary.txt";
             using (StreamWriter writer = new StreamWriter(filePath))
             {
                 writer.WriteLine("Summary Report");
                 writer.WriteLine($"Total number of students: {totalStudents}");
                 writer.WriteLine($"Average age of students: {averageAge:F2}");
             }
- 
-            MessageBox.Show("Summary saved to ReportSummary.txt", "Summary Report", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
- 
-        private void btnBackToDatabase_Click(object sender, EventArgs e)
-        {
- 
-            this.Close();
-        }
-    }
 
+        
+        public (int TotalStudents, double AverageAge) GetSummaryData()
+        {
+            int totalStudents = students.Count;
+            double averageAge = students.Any() ? students.Average(s => s.Age) : 0;
+            return (totalStudents, averageAge);
+        }
     }
 }
